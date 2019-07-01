9b19c3dac81cb124beed319e733a0f5888c5a85c
Chien <brad@brad.tw>
2019-07-01 17:57:46 +0800

    201907011757

--- brad17.php
+++ brad17.php
@@ -1,5 +1,16 @@
+<?php
+    $n = $sum = '';
+    if (isset($_GET['n'])){
+        $n = $_GET['n'];
+        $sum = 0;
+        $i = 1;
+        while ($i <= $n) $sum += $i++;
+        
+    }
+?>
 <form action="brad17.php">
 1 + 2 + ... +
-<input type="number" name="n">
+<input type="number" name="n" value="<?php echo $n; ?>">
 <input type="submit" value="=">
+<?php echo $sum; ?>
 </form>
